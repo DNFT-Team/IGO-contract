354c3572c8f384058fcd63d6f571445452113766
Jat <3346136+xiaojat@users.noreply.github.com>
2021-08-04 18:42:37 +0800

    feature:raffle

--- contracts/IGO.sol
+++ contracts/IGO.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract IGO {
+contract IGO is ERC1155Holder, Ownable {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
@@ -22,6 +24,7 @@ contract IGO {
   }
 
   uint constant Decimals = 1e18;
+  uint256 public amount;
   /// @dev address , token1155.ids
   /// @dev address,tokenID, only mint once
   mapping(address => uint256) public rewards;
@@ -30,43 +33,66 @@ contract IGO {
   //@dev token1155.id
   uint256[] public ids;
 
+  event Raffled(address indexed user, uint256 nftId,uint256 timestamp);
 
-  constructor(address _tokenAddress, address _nftAddress, uint256[] memory _ids) {
+  constructor(address _tokenAddress,uint256 _amount, address _nftAddress, uint256[] memory _ids) {
     payToken = IERC20(_tokenAddress);
     nftToken = IERC1155(_nftAddress);
+    amount = _amount;
     ids = _ids;
-    for(uint256 i=0; i<_ids.length ;i++){
+    for(uint256 i = 0; i < _ids.length; i++){
       tokenIds[_ids[i]] = TokenTotal(_ids[i],0,0,true);
     }
   }
 
-  function mint(uint256 _id) public {
+  function raffle(uint256 _id) public {
     console.log("mint '%s' '%s'",msg.sender,_id);
     require(rewards[msg.sender]==0, "already received");
 
-    /// @dev total rule
     tokenIds[_id].mintedTotal = tokenIds[_id].mintedTotal.add(1);
     require(tokenIds[_id].total >= tokenIds[_id].mintedTotal,"max total");
-    /// TODO random number
-    /// TODO call 1155
-    /// TODO payable
+
+    payToken.safeTransferFrom(msg.sender, address(this), amount);
+
+    nftToken.safeTransferFrom(address(this),msg.sender,_id,1,"0x00");
+    //TODO random
 
     rewards[msg.sender] = _id;
     console.log("success");
+
+    emit Raffled(msg.sender, _id, block.number);
   }
 
   function setRewardTotal(uint256 _id, uint256 _total) external  {
     require(tokenIds[_id].isValid==true,"invalid id");
     require(tokenIds[_id].total==0,"already been set up");
-    tokenIds[_id].total =  _total;
+    tokenIds[_id].total = _total;
     console.log("set total",_id,_total);
   }
 
   function idsCount() external view returns (uint256) {
      return ids.length;
- }
+   }
 
  function balance() public view returns (uint256){
-       return payToken.balanceOf(address(this));
+     return payToken.balanceOf(address(this));
+ }
+
+ function balanceOf(address _addr) external view returns (uint256){
+     return payToken.balanceOf(_addr);
+ }
+
+ function nftBalance(uint256 _id) external view returns (uint256){
+    return nftToken.balanceOf(address(this),_id);
+ }
+
+ function nftBalanceOf(address _address,uint256 _id) external view returns (uint256){
+  return nftToken.balanceOf(_address,_id);
+ }
+
+ function withdraw() external onlyOwner {
+   require(balance()>0,"balance zero");
+   payToken.safeTransfer(owner(), balance());
+
  }
 }
--- scripts/igo-script.js
+++ scripts/igo-script.js
@@ -5,9 +5,19 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+let token;
+let nft;
+async function deployToken() {
+  const Token20 = await hre.ethers.getContractFactory("Token20");
+  token = await Token20.deploy(10000);
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
+  return token.address;
+}
+
 async function deployNFT() {
   const NFT = await hre.ethers.getContractFactory("DNFNft");
-  const nft = await NFT.deploy();
+  nft = await NFT.deploy();
   await nft.deployed();
   console.log("NFT1155 deployed to:", nft.address);
   return nft.address;
@@ -21,23 +31,26 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // await deployNFT();
+  let erc20 = await deployToken();
+  let nft1155 = await deployNFT();
   // return ;
 
-  const erc20 = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const nft1155 = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  // const erc20 = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const amount = 12
+  // const nft1155 = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
   const ids = [100,200,300]
+  const ids_count= [50,950,4000]
 
   // We get the contract to deploy
   const [owner, addr1, addr2] = await ethers.getSigners();
   const IGO = await hre.ethers.getContractFactory("IGO");
-  const igo = await IGO.deploy(erc20,nft1155,ids);
+  const igo = await IGO.deploy(erc20,amount,nft1155,ids);
 
   await igo.deployed();
 
   console.log("IGO deployed to:", igo.address);
-
-  console.log("balance",await igo.balance());
+  console.log("balanceOfOwner",(await igo.balanceOf(owner.address)).toString());
+  console.log("balanceOfIgo",(await igo.balance()).toString());
 
   let idsCount =Number( await igo.idsCount());
   console.log("idsCount=",idsCount.toString());
@@ -51,28 +64,36 @@ async function main() {
     console.log("token>> ",tokenId,tokenObj.toString());
   }
 
-  console.log("mint",ids[0],await igo.mint(ids[0]));
+  await token.approve(igo.address,12);
+  console.log("mint nft1155>>>");
+  await nft.mint(igo.address,ids[0],ids_count[1],"0x0000000000000000000000000000000000000000000000000000000000000000");
+  console.log("nftOfOwner",(await nft.balanceOf(owner.address,ids[0])).toString());
+  console.log("nftOfIGO",(await nft.balanceOf(igo.address,ids[0])).toString());
+
+  console.log("raffle",ids[0],await igo.raffle(ids[0]));
   console.log("reward tokenId",owner.address,(await igo.rewards(owner.address)).toString());
 
-  //only mint once
-  await igo.mint(ids[1]);
+  //only raffle once
+  // await igo.raffle(ids[1]);
 
   for(var i=0;i<idsCount;i++){
     let tokenId = (await igo.ids(i)).toString();
     let tokenObjNew = await igo.tokenIds(tokenId);
     console.log("token>>> ",tokenId,tokenObjNew.toString());
   }
+  console.log("nftOfOwner",(await nft.balanceOf(owner.address,ids[0])).toString());
+  console.log("nftOfIGO",(await nft.balanceOf(igo.address,ids[0])).toString());
 
+  console.log("balanceOfOwner",(await igo.balanceOf(owner.address)).toString());
+  console.log("balanceOfIgo",(await igo.balance()).toString());
 
-
-
+  console.log("exec withdraw***********************************************");
+  await igo.withdraw();
+  console.log("balanceOfOwner",(await igo.balanceOf(owner.address)).toString());
+  console.log("balance",(await igo.balance()).toString());
 }
 
-async function getToken(igo,index)
-{
 
-
-}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
